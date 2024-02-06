b6275a34b9e5390f3611e630f2a54e20c46305fb
Anthony Bui <anthonyb50@gmail.com>
2024-02-06 09:46:20 -0500

    Class practice

--- 03-IntroductionToJavaPlatform/Planet.java
+++ 03-IntroductionToJavaPlatform/Planet.java
@@ -4,8 +4,13 @@ class Planet {
          System.out.println("Revolve");
     }
 
+    void gravity() {
+        System.out.println("Attraction and Repulsion");
+    }
+
     public static void main(String[] args) {
     	Planet earth = new Planet();
     	earth.revolve();
+        earth.gravity();
     }
 }
