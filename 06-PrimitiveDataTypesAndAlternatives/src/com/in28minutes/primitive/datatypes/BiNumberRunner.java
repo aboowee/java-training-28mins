f456dfef3debe8a11375eb51162f64856df9a49d
Anthony Bui <anthonyb50@gmail.com>
2024-02-15 10:31:48 -0500

    Division runner

--- 06-PrimitiveDataTypesAndAlternatives/src/com/in28minutes/primitive/datatypes/BiNumberRunner.java
+++ 06-PrimitiveDataTypesAndAlternatives/src/com/in28minutes/primitive/datatypes/BiNumberRunner.java
@@ -4,12 +4,13 @@ public class BiNumberRunner {
 
 	public static void main(String[] args) {
 		BiNumber numbers = new BiNumber(2, 3);
-		
+
 		System.out.println(numbers.add());//2+3
 		System.out.println(numbers.multiply());//2*3
-		
-		numbers.doubleValue();//Double both numbers 
-		
+		System.out.println(numbers.divide());//2/3 NOTE: Rounds to 0 due to being int
+
+		numbers.doubleValue();//Double both numbers
+
 		System.out.println(numbers.getNumber1());//4
 		System.out.println(numbers.getNumber2());//6
 	}
